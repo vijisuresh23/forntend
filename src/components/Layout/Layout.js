42fce7aff04958122135f2fa44e2ed42913e6cbe
Abhinav Manchanda <abhinav@abhinav.local>
2020-03-27 14:54:16 +0530

    Added a body container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Box } from "@material-ui/core";
+import { Box, Container, Paper } from "@material-ui/core";
 import Header from "../Header/Header";
+import styles from "./LayoutStyles";
 
 const Layout = () => {
+    const classes = styles();
     return (
         <Box>
             <Header />
+            <Container maxWidth={"lg"} className={classes.container}>
+                <Paper className={classes.paper}>
+                </Paper>
+            </Container>
 
         </Box>
     )
---
+++ src/components/Layout/LayoutStyles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/core";
+
+export default makeStyles({
+  paper: {
+    padding: "15px 30px",
+    minHeight: "400px"
+  },
+  container: {
+    padding: "40px 0"
+  }
+});
