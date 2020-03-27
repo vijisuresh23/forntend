17dc1466c83dea840354c72d1416ccfab3b2b967
Abhinav Manchanda <abhinav@abhinav.local>
2020-03-27 13:07:42 +0530

    Added MIUI, Created a the basic structure of app, removed app test.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, CssBaseline } from '@material-ui/core';
+import Layout from "./components/Layout/Layout";
+import Theme from './Theme';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={Theme}>
+      <CssBaseline />
+      <Layout />
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Box} from "@material-ui/core";
+
+const Layout = () => {
+    return(
+        <Box>
+        </Box>
+    )
+}
+
+export default Layout;
