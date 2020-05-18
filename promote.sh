eaa441d403de79258d899b0ad80294966381039b
Gaurav <gauravmi@thoughtworks.com>
2020-05-18 09:54:37 +0530

    [Gaurav] add promote staging stage

---
+++ promote.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+set -v
+
+export PROMOTE_FROM=$1
+export IMAGE=$BOOKING_WEB_IMAGE_NAME:$CI_COMMIT_SHORT_SHA-$PROMOTE_FROM
+
+echo "Pomoting images - $IMAGE"
+
+docker -v login -u gitlab-ci-token -p "$GITLAB_REGISTRY_PASSWORD" registry.gitlab.com
+
+docker pull "$IMAGE"
+docker tag "$IMAGE" "$BOOKING_WEB_IMAGE_NAME:$CI_COMMIT_SHORT_SHA-$CI_ENVIRONMENT_SLUG"
+docker push "$BOOKING_WEB_IMAGE_NAME:$CI_COMMIT_SHORT_SHA-$CI_ENVIRONMENT_SLUG"
+
+set +v
+
