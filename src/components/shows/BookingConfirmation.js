307c0e63acf343ea3ecfb2be63320a33d2d57e22
gauravmi1 <gmi3024@gmail.com>
2020-06-20 01:51:21 +0530

    [Gaurav] show success message on booking confirmation

--- src/components/shows/BookingConfirmation.js
+++ src/components/shows/BookingConfirmation.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Dialog, DialogContent, Typography } from "@material-ui/core";
+import { Dialog, Snackbar, DialogContent, Typography } from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert/Alert";
 import styles from "./styles/customerDetailsDialogStyles"
 
-const BookingConfirmation = ({ bookingConfirmation, showConfirmation, onClose}) => {
+const BookingConfirmation = ({ bookingConfirmation, showConfirmation, onClose }) => {
     const classes = styles();
     const handleClose = () => {
         onClose();
@@ -10,30 +11,33 @@ const BookingConfirmation = ({ bookingConfirmation, showConfirmation, onClose}) 
 
     return (
         <Dialog open={showConfirmation} onClose={handleClose}>
-        <Typography variant="h6" className={classes.dialogHeader}>
-            Booking Confirmation
+            <Alert severity="success">
+                Seats booked successfully!
+                </Alert>
+            <Typography variant="h6" className={classes.dialogHeader}>
+                Booking Confirmation
         </Typography>
-        <DialogContent>
-            <Typography variant="body1" display="block" gutterBottom>
-                Booking id : {bookingConfirmation.id}
-            </Typography>
-            <Typography variant="body1" display="block" gutterBottom>
-                Show Date: {bookingConfirmation.showDate}
-            </Typography>
-            <Typography variant="body1" display="block" gutterBottom>
-                Show start time: {bookingConfirmation.startTime}
-            </Typography>
-            <Typography variant="body1" display="block" gutterBottom>
-                Customer Name: {bookingConfirmation.customerName}
-            </Typography>
-            <Typography variant="body1" display="block" gutterBottom>
-                Amount Paid: {bookingConfirmation.amountPaid}
-            </Typography>
-            <Typography variant="body1" display="block" gutterBottom>
-                Number of seats booked: {bookingConfirmation.noOfSeats}
-            </Typography>
-        </DialogContent>
-    </Dialog>
+            <DialogContent>
+                <Typography variant="body1" display="block" gutterBottom>
+                    Booking id : {bookingConfirmation.id}
+                </Typography>
+                <Typography variant="body1" display="block" gutterBottom>
+                    Show Date: {bookingConfirmation.showDate}
+                </Typography>
+                <Typography variant="body1" display="block" gutterBottom>
+                    Show start time: {bookingConfirmation.startTime}
+                </Typography>
+                <Typography variant="body1" display="block" gutterBottom>
+                    Customer Name: {bookingConfirmation.customerName}
+                </Typography>
+                <Typography variant="body1" display="block" gutterBottom>
+                    Amount Paid: {bookingConfirmation.amountPaid}
+                </Typography>
+                <Typography variant="body1" display="block" gutterBottom>
+                    Number of seats booked: {bookingConfirmation.noOfSeats}
+                </Typography>
+            </DialogContent>
+        </Dialog>
     )
 }
 
